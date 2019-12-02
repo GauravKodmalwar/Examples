159b786c81fc7ebf9aa7622842cd31136871a8d3
GauravKodmalwar <46324228+GauravKodmalwar@users.noreply.github.com>
2019-12-02 22:13:50 +0530

    Initial commit

---
+++ Day1/Topic3_PythonScripting.py
@@ -0,0 +1,46 @@
+# Single line comments are created using hash ==> #
+
+"""
+Multi line comments are generated in blocks
+"""
+
+# Basic output in Python
+print("Hello")
+
+# whitespace in python or also called Indentation
+# In some languages its called blocks like () or {}
+# In python, any number of space or spaces can be whitespace
+# It has to be equal in number for code in same line
+if(False):
+    print("Sucess")
+else:
+    print("Failed")
+
+
+# Line termination in Python
+print("This topic will show, how to print a new line in Python and using it, you can also understand the purpose of it. Sometimes, "
+      "you may need to add new line in output")
+
+print('In Python, even "single quotes" can be used ')
+
+print("This topic will show, how to print a new line in Python and using it, you can also understand the purpose of it. Sometimes, \r\n"
+      "you may need to add new line in output")
+
+# Simple user input can be with function input, thats it. :)
+age = input("enter your age")
+print("your age is " + age + " years")
+
+# Module is just a python file with .py extension
+# It can contain, variable or clsses or function or code
+import myModule
+myModule.sayHello("Gaurav", "Pythoner")
+
+
+# Module Search path
+# Current directory then sys.path
+# Now sys module is not here in current directory, its imported from site-packages
+import sys
+print("System search path can be found here: \n", "\n".join(sys.path))
+
+import json
+print(json.__file__)
---
+++ Day1/myModule.py
@@ -0,0 +1,2 @@
+def sayHello(name, name2):
+    print("Hello {0}, how are you? Having a good learning, {1} :) ".format(name, name2))
