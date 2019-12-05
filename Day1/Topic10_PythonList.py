7c6b2ea70d3ed254c3933b5ea8ba83d6fd17d69d
singak <gaurav.kodmalwar@gmail.com>
2019-12-05 16:47:26 +0530

    Updated with module10

--- Day1/Topic10_PythonList.py
+++ Day1/Topic10_PythonList.py
@@ -20,14 +20,37 @@ print("addition in list ", varList + [5])
 print("multiplication in list ", varList * 2)
 
 # String indexing
+abc = "Python training"
+for i in range(len(abc)):
+    print(abc[i])
 
+print(abc[5])
 
 # String slicing
-
+print(abc[4:9:-1])
 
 # String iteration
+# define a list
+my_list = [4, 7, 0, 3]
 
+# get an iterator using iter()
+my_iter = iter(my_list)
+# fetch next value from iterator using next
+print(next(my_iter))
 
-# Multi dimensional List
+print("print values using for loop")
+for element in my_iter:
+    print(element)
 
+# Multi dimensional List: https://www.geeksforgeeks.org/multi-dimensional-lists-in-python/
+a = [[2, 4, 6, 8 ], [ 1, 3, 5, 7 ], [ 8, 6, 4, 2 ], [ 7, 5, 3, 1 ]]
 
+print("fetching list using one by one array ==> ")
+for record in a:
+    print(record)
+
+print("print multi dimensional list using element by element")
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        print(a[i][j], end=" ")
+    print()
