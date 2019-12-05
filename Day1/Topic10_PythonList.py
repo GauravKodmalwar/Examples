f687390fcb2f44fe20a24b2281d64d53f5538917
singak <gaurav.kodmalwar@gmail.com>
2019-12-05 12:01:00 +0530

    Updated with module10

---
+++ Day1/Topic10_PythonList.py
@@ -0,0 +1,33 @@
+#List are mutable
+varList = [2, 3, 5 , 15.2]
+print(id(varList))
+varList[2] = "python"
+print(id(varList))
+
+# Common List method: https://www.programiz.com/python-programming/methods/list
+varList.append(25)
+varList.extend([24, 65, 77, 92])
+print(varList)
+print(varList.pop(3))
+print(varList)
+print(zip(varList))
+
+# range function
+
+
+# List operations
+print("addition in list ", varList + [5])
+print("multiplication in list ", varList * 2)
+
+# String indexing
+
+
+# String slicing
+
+
+# String iteration
+
+
+# Multi dimensional List
+
+
---
+++ Day1/Topic9_PythonTuples.py
@@ -0,0 +1,46 @@
+# Immutable : https://www.w3schools.com/python/python_tuples.asp
+varTuple = (5, 20, 40)
+#varTuple[0] = 5  # TypeError: 'tuple' object does not support item assignment
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+
+#Common tuples methods
+thistuple = ("apple", "banana", "cherry")
+print("count function returns the number of counts in tuples = ", thistuple.count("apple"))
+print("index returns the index of object in a tuple = ", thistuple.index("cherry"))
+
+#Tuples operations: https://www.geeksforgeeks.org/tuples-in-python/
+a = (3, 56, 20, 10)
+b = (12.4, 567.2, 565.357)
+print("addition of tuples ", a + b)
+print("multiplication of tuples ", a * 5)
+print("smalles number in tuple: ", min(b))
+
+del a
+#print(a)
+
+#Tuples indexing
+print("Indexing tuple just like list")
+print(thistuple[1])
+
+#Tuples slicing
+print("tuple values after reversing using slicing: ", thistuple[::-1])
+
+#Tuples iteration
+for i in range(len(thistuple)):
+    print(thistuple[i])
+
+#Multi dimensional tuples
+multiTuples = ((25, 60), ("tuple1", "tuple2"))
+print(multiTuples)
+print(multiTuples[1])
+
+"""
+************* Difference between list and tuple *************
+1) List is mutable i.e once created then we can modify its contents. Whereas, a Tuple is immutable i.e. once created we can change its contents, therefore tuple doesn’t provide functions like append(), remove() etc.
+2) As tuple is immutable, so we can use it as key in a dictionary too.
+"""
