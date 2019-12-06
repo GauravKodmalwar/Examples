965f02da9b686262fb8c104ce20da6ef0848ee6b
singak <gaurav.kodmalwar@gmail.com>
2019-12-06 14:09:21 +0530

    Updated with functions module

---
+++ Day2/Topic2_PythonFunctions.py
@@ -0,0 +1,43 @@
+# function basics
+"""
+Step 1: Declare the function with the keyword def followed by the function name.
+Step 2: Write the arguments inside the opening and closing parentheses of the function, and end the declaration with a colon.
+Step 3: Add the program statements to be executed.
+"""
+
+def my_function():
+  print("*** Hello from a function ***")
+my_function()
+
+def my_function2(name):
+  print("*** Hello {}, from a function ***".format(name))
+
+my_function2("Gaurav")
+
+# Function polymorphism
+def my_function(name):
+  print("*** Hello {}, from a function ***".format(name))
+
+def my_function(name, age):
+  print("*** Hello {}, from a function. Your age is {} ***".format(name, age))
+
+my_function("Gaurav", 30)
+
+# Argument defaults. Argument assignment happens with = sign to a argument. https://www.geeksforgeeks.org/polymorphism-in-python/
+def my_function(name, age, gender="male"):
+  print("*** Hello {}, from a function. Your age is {}. Your gender is {} ***".format(name, age, gender))
+
+my_function("Gaurav", 30)
+my_function("Gaurav", 30, "female")
+
+# Lambada function
+"""
+A lambda function is a small anonymous function.
+A lambda function can take any number of arguments, but can only have one expression.
+"""
+x = lambda a : a + 10
+print(x(5))
+
+x = lambda a, b : a * b
+print(x(5, 6))
+
