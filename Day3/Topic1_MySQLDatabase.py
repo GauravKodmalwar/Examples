89535379653933b3c22b72a8410c085e57f953ca
GauravKodmalwar <46324228+GauravKodmalwar@users.noreply.github.com>
2019-12-11 00:01:50 +0530

    Updated with classes

--- Day3/Topic1_MySQLDatabase.py
+++ Day3/Topic1_MySQLDatabase.py
@@ -3,20 +3,19 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
   host="localhost",
-  user="yourusername",
-  passwd="yourpassword"
+  user="python",
+  passwd="python",
+  database="PythonTraining"
 )
 print(mydb)
 
 # Creating database & table
 mycursor = mydb.cursor()
-mycursor.execute("CREATE DATABASE mydatabase")
+#mycursor.execute("CREATE DATABASE mydatabase2")
 
 mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
 
 # Insert operation
-mycursor = mydb.cursor()
-
 sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
 val = ("John", "Highway 21")
 mycursor.execute(sql, val)
@@ -25,12 +24,15 @@ mydb.commit()
 print(mycursor.rowcount, "record inserted.")
 
 # Read\Update\Delete\Commit\Rollback operations
-mycursor = mydb.cursor()
-mycursor.execute("SELECT * FROM customers")
-myresult = mycursor.fetchall()
-
 sql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'"
 mycursor.execute(sql)
 
 sql = "DELETE FROM customers WHERE address = 'Mountain 21'"
 mycursor.execute(sql)
+
+mycursor.execute("SELECT * FROM customers")
+myresult = mycursor.fetchall()
+print(myresult)
+
+mycursor.close()
+mydb.close()
