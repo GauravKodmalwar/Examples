6e0fd94d0aab16e7d41f9d1d787ad99018ecb553
GauravKodmalwar <46324228+GauravKodmalwar@users.noreply.github.com>
2019-12-10 17:29:15 +0530

    Updated with classes

--- Day2/Topic3_ClassesAndObjects.py
+++ Day2/Topic3_ClassesAndObjects.py
@@ -1,9 +1,8 @@
 # Variable modifiers: https://www.tutorialsteacher.com/python/private-and-protected-access-modifiers-in-python
 """
-There are three types of access modifiers in Python: public, private, and protected.
-1) Variables with the public access modifiers can be accessed anywhere inside or outside the class.
-2) the private variables can only be accessed inside the class.
-3) while protected variables can be accessed within the same package.
+There are two types of access modifiers in Python: public, private.
+1) Variables\functions with the public access modifiers can be accessed anywhere inside or outside the class.
+2) while protected variables can be accessed within the same package.
 """
 # Introduction to OOP using Python: https://www.programiz.com/python-programming/object-oriented-programming
 """
